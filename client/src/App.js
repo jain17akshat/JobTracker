e09c67624aa4f41948706f7fb9ed63c3c1410d51
jain17akshat <jain.7akshat@gmail.com>
2025-07-11 23:20:15 +0530

    Set up routing with React Router and add Login, Register, and Dashboard pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-   <Router>
-
-    
-   </Router>
-
-
-   
+    <Router>
+      <Routes>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </Router>
   );
 }
 
